1afb68a186cb2e5418619b489ca2d0999222a5d3
natanaeldeholanda <natan.holanda@gmail.com>
2024-06-04 16:47:05 -0300

    Assisti até o vídeo 34.,

---
+++ src/com/abctreinamentos/siscapacit/SiscapacitApp.java
@@ -0,0 +1,18 @@
+package com.abctreinamentos.siscapacit;
+import java.util.LinkedList;
+import java.util.List;
+/*
+ * Autor: Natanael de Holanda
+ * Versão: 1.0
+ * Descrição: Classe que representa o Servidor Público
+ */
+
+// Atributos
+public class SiscapacitApp {
+    List<ServidorPublico> sevidores = new LinkedList<>();
+    List<Curso> cursos = new LinkedList<>();
+    public static void main(String[] args) {
+
+    }
+}
+
